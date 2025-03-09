f1ffc51d2106b3bf74cd805762807d5a47f553c3
bk <bakr.frag@gmail.com>
2025-03-10 01:51:50 +0200

    include rate limitimg globally

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
 from apis.users import user_router
-from apis.prtoducts import product_router
+from apis.products import product_router
 from core.logger import setup_logging
 
 setup_logging()
+limiter = Limiter(key_func= get_remote_address, default_limits=["10/minute"])
 app = FastAPI(root_path="/api")
+app.state.limiter = limiter
+app.add_middleware(SlowAPIMiddleware)
 app.include_router(user_router)
 app.include_router(product_router)
 
