2f431b7225f14b7a2fb4830f677c5d224226cdc3
bk <bakr.frag@gmail.com>
2025-03-09 21:41:45 +0200

    add user models

---
+++ src/db/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from base import Base, engine
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
