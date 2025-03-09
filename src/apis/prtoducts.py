e83002a97c04b23d588988427bad77cd1c7a7f00
bk <bakr.frag@gmail.com>
2025-03-10 00:40:04 +0200

    include utility to adjust price of product as per stock quantity

--- src/apis/prtoducts.py
+++ src/apis/prtoducts.py
@@ -5,3 +5,4 @@ from apis.schemas import ProductCreate, ProductResponse
 
 
 product_router = APIRouter()
+
--- src/core/commons.py
+++ src/core/commons.py
@@ -10,3 +10,16 @@ def get_secret_key(key_name: str) -> str:
         key_name (str): key name
     """
     return os.getenv(key_name)
+
+def adjust_price(price: float, stock_quantity: int):
+    """
+    adjust product price as per stock quantity
+    if stock_quantity < 10: price increase by 7%
+    else: price decrease by 5%
+    
+    Args:
+        price (float): represents product price 
+        stock_quantity: represents stock quantity of product 
+    """
+    return (price + (price * 0.07)) if stock_quantity < 10 else (price - (price * 0.07))
+    
