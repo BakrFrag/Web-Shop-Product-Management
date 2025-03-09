a4b6ca6a15ea0a1389c31ad49e699d1906608ef1
bk <bakr.frag@gmail.com>
2025-03-10 01:39:54 +0200

    implement a way to make sure user is login while interact with products

--- src/apis/prtoducts.py
+++ src/apis/prtoducts.py
@@ -4,39 +4,42 @@ from sqlalchemy.orm import Session
 from db.db import get_db
 from apis.queries import create_product, get_product_by_id, get_products_list
 from apis.schemas import ProductCreate, ProductResponse
-
+from users.auth import get_current_user
 
 product_router = APIRouter()
 
 
 @product_router.post("/products/", tags = ["products"], response_model = ProductResponse)
-def create_new_product(product: ProductCreate, db: Session = Depends(get_db)):
+async def create_new_product(product: ProductCreate, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     """
     create new product
     Args:
         product (ProductCreate): data to create and add new product
         db (Session): db session object
+        current_user: current authenticated user
     """
     return create_product(db, product)
 
 
 @product_router.get("/products/", tags = ["products"], response_model = List[ProductResponse])
-def list_all_products(db: Session = Depends(get_db)):
+async def list_all_products(db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     """
     list and get all products with their price adjusted as stock_quantity
     Args:
         db (Session): db session object
+        current_user: current login user
     """
     return get_products_list(db)
 
 
 @product_router.get("/products/{product_id}/", tags = ["products"], response_model = ProductResponse)
-def get_product(product_id: int, db: Session = Depends(get_db)):
+async def get_product(product_id: int, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     """
     get product by id, product price will be adjusted as stock quantity
     Args:
         db (session): db session object 
         product_id (int): id of project, parsed as url variable
+        current_user: current login user
     Raises:
         HTTPException: if product not exits     
     """
