8bd4df612309f40e1b5a52bf69c7b645a87db29e
bk <bakr.frag@gmail.com>
2025-03-10 01:21:10 +0200

    include products end points

--- src/apis/prtoducts.py
+++ src/apis/prtoducts.py
@@ -1,8 +1,46 @@
+from typing import List
 from fastapi import Depends, HTTPException, APIRouter, status
+from sqlalchemy.orm import Session
 from db.db import get_db
-from apis.queries import create_product, get_product_by_id, get_all_products
+from apis.queries import create_product, get_product_by_id, get_products_list
 from apis.schemas import ProductCreate, ProductResponse
 
 
 product_router = APIRouter()
 
+
+@product_router.post("/products/", tags = ["products"], response_model = ProductResponse)
+def create_new_product(product: ProductCreate, db: Session = Depends(get_db)):
+    """
+    create new product
+    Args:
+        product (ProductCreate): data to create and add new product
+        db (Session): db session object
+    """
+    return create_product(db, product)
+
+
+@product_router.get("/products/", tags = ["products"], response_model = List[ProductResponse])
+def list_all_products(db: Session = Depends(get_db)):
+    """
+    list and get all products with their price adjusted as stock_quantity
+    Args:
+        db (Session): db session object
+    """
+    return get_products_list(db)
+
+
+@product_router.get("/products/{product_id}", tags = ["products"], response_model = ProductResponse)
+def get_product(product_id: int, db: Session = Depends(get_db)):
+    """
+    get product by id, product price will be adjusted as stock quantity
+    Args:
+        db (session): db session object 
+        product_id (int): id of project, parsed as url variable
+    Raises:
+        HTTPException: if product not exits     
+    """
+    product = get_product_by_id(db, product_id)
+    if not product:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="Product not found")
+    return product
--- src/apis/queries.py
+++ src/apis/queries.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import
+from sqlalchemy import Case
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from db.models import Product, User
@@ -52,14 +52,64 @@ def create_product(db: Session, name: str, stock_quantity: int, description: str
     return product
 
 def get_product_by_id(db: Session, id: int):
-    
-    return db.query(User).filter(Product.id == id).first()
-
-def get_all_products(db: Session):
     """
-    get list of all products
+    get product by id
+    adjust price of product as
+    if stock_quantity = 0, return original price 
+    if stock_quantity > 0 and stock_quantity <=5, increase price by 5 %
+    if stock_quantity > 5 decease price by 7%
+    
     Args:
-        db(Session): db session object  
+        db(Session): session object to interact with db 
+        id (int): product id
     """
-    return db.query(Product).all()
-    
+    product = (
+        db.query(
+            Product.id,
+            Case(
+                (Product.stock_quantity == 0, Product.price),  # If stock is 0, return original price
+                (Product.stock_quantity <= 5, Product.price * 1.05),  # If stock <= 5, increase by 5%
+                else_=Product.price * 0.93,  #  if stock_quantity > 5, decease by 7%
+            ).label("dynamic_price"),
+            Product.stock_quantity,
+            Product.description,
+            Product.name
+        ).filter(Product.id = id ).first()
+    )
+    return {
+        "id": id, "name": product.name, "description": product.description, "price": round(product.dynamic_price, 2), "stock_quantity": product.stock_quantity
+    } if product else None
+
+
+def get_products_list(db: Session):
+    """
+    get all products with price adjust as 
+    if stock_quantity = 0, return original price 
+    if stock_quantity > 0 and stock_quantity <=5, increase price by 5 %
+    if stock_quantity > 5 , decease price by 7%
+    """
+    products = (
+        db.query(
+            Product.id,
+            Case(
+                (Product.stock_quantity == 0, Product.price),  # If stock is 0, return original price
+                (Product.stock_quantity <= 5, Product.price * 1.05),  # If stock <= 5, increase by 5%
+                else_=Product.price * 0.93,  #  if stock_quantity > 5, decease by 7%
+            ).label("dynamic_price"),
+            Product.stock_quantity,
+            Product.description,
+            Product.name
+        )
+        .all()
+    )
+
+    return [
+        {
+            "id": product.id,
+            "name": product.name,
+            "price": round(product.dynamic_price, 2),
+            "stock": product.stock,
+            "description": product.description,
+        }
+        for product in products
+    ] if products else []
